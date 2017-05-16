cbd006cdbf0c5c5b80ee7a0b115bf8a8a8c3d977
Jonathan <Razordor@gmail.com>
2017-05-16 15:43:49 -0700

    Update Source.cpp

--- Source.cpp
+++ Source.cpp
@@ -1,6 +1,57 @@
+//Jonathan Thomason
+//[name here]
+
 #include <fstream>
 #include <iostream>
 
+//prototypes
+void search(), 
+delete(), 
+change(), 
+append(), 
+display();
+
 int main() {
-	return 0;
+	//welcome message
+	cout << "Welcome to cutomer accounts record\n," 
+		<< "the safest, securest account application a novice programmer\n"
+		<< "can code. Please enter a number below " << endl;
+	do
+	{
+		cout << "=====[MENU]=====" << endl
+			<< ""
+			
+		switch(key)
+		{
+			case 1:
+				break;
+			case 2:
+				break;
+			case 3:
+				break;
+			case 4;
+				break
+		}
+	} while (true)
+}//end main
+
+void search()
+{
+	//code here
+}
+void delete()
+{
+	//code here
+}
+void change()
+{
+	//code here
+}
+void append()
+{
+	//code here
+}
+void display()
+{
+	//code here
 }
